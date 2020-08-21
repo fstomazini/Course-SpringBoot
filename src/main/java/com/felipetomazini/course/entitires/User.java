a3a37c95b82da91ef6f1cfd016105723db9a9b3f
fstomazini <felipestomazini@gmail.com>
2020-08-21 15:22:19 -0300

    h2-Database and test profile

--- src/main/java/com/felipetomazini/course/entitires/User.java
+++ src/main/java/com/felipetomazini/course/entitires/User.java
@@ -1,9 +1,16 @@
 package com.felipetomazini.course.entitires;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String Email;
@@ -21,6 +28,7 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    @Id
     public Long getId() {
         return id;
     }
