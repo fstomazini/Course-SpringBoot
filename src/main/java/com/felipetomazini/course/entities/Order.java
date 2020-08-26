c191717a6cc74bf18af66d94dd94a2e3aa787488
fstomazini <felipestomazini@gmail.com>
2020-08-26 19:41:26 -0300

    Entidade Payment

--- src/main/java/com/felipetomazini/course/entities/Order.java
+++ src/main/java/com/felipetomazini/course/entities/Order.java
@@ -31,6 +31,8 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
 
     private Set<OrderItem> items = new HashSet<>();
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
     }
@@ -58,6 +60,14 @@ public class Order implements Serializable {
 
     public OrderStatus getOrderStatus() {
         return OrderStatus.valueOf(orderStatus);
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public void setId(Long id) {
---
+++ src/main/java/com/felipetomazini/course/entities/Payment.java
@@ -0,0 +1,51 @@
+package com.felipetomazini.course.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment(Long id, Instant moment, Order order) {
+        this.id = id;
+        this.moment = moment;
+        this.order = order;
+    }
+
+    public Payment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Payment)) return false;
+        Payment payment = (Payment) o;
+        return Objects.equals(getId(), payment.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
