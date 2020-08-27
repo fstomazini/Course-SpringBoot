334a0e522b2aab880ff5f944887e2063a782cd85
fstomazini <felipestomazini@gmail.com>
2020-08-26 23:43:32 -0300

    Exception handling delete

--- src/main/java/com/felipetomazini/course/services/UserService.java
+++ src/main/java/com/felipetomazini/course/services/UserService.java
@@ -10,6 +10,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,12 +43,18 @@ public class UserService {
             throw new DatabaseExcepetion(e.getMessage());
         }
     }
-    public User update(Long id, User obj){
-        User entity = userRepository.getOne(id);
-        updateData(entity,obj);
-        return userRepository.save(entity);
-    }
+    public User update(Long id, User obj) {
 
+        try {
+            User entity = userRepository.getOne(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        }catch (EntityNotFoundException e){
+
+            throw new ResourceNotFoundException(id);
+        }
+
+    }
     private void updateData(User entity, User obj) {
 
         entity.setName(obj.getName());
